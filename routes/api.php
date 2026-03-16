39b0477f68756989edd4bd679aa10d36a6fc0bce
Khuzaima <khuzaima8855.za@gmail.com>
2026-03-16 20:10:48 +0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ShopifyController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return redirect('/app');
+});
+
+// 1. Install request from merchant
+Route::get('/api/auth', [ShopifyController::class, 'redirectToShopify']);
+
+// 2. OAuth callback from Shopify
+Route::get('/api/auth/callback', [ShopifyController::class, 'handleCallback']);
+
+// 3. React App entry point (iframe app)
+Route::get('/app', function () {
+    return view('app');
+});
